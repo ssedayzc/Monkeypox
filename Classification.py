5d00599ba8e62ff3f1824dcfdfb64fc18d299435
ssedayzc <seda.yazici08@erzurum.edu.tr>
2023-11-15 01:21:27 +0300

    update

--- Classification.py
+++ Classification.py
@@ -1,67 +1,89 @@
-# Gerekli kütüphaneleri yükleyin
 import os
+import random
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
-from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.feature_selection import SelectKBest, f_classif
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.vgg16 import preprocess_input
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.models import Model
+from sklearn.preprocessing import LabelEncoder
 
-# Veri setinin bulunduğu ana klasör
-data_folder = 'C:/Users/lenovo/Downloads/Augmented Images/Augmented Images'
+# Veri setinin yüklenmesi ve özellik vektörlerinin oluşturulması
+def load_and_preprocess_images(folder, model):
+    images = []
+    labels = []
+    for class_folder in os.listdir(folder):
+        class_path = os.path.join(folder, class_folder)
+        if os.path.isdir(class_path):
+            for filename in os.listdir(class_path):
+                img_path = os.path.join(class_path, filename)
+                img = image.load_img(img_path, target_size=(224, 224))
+                img_array = image.img_to_array(img)
+                img_array = np.expand_dims(img_array, axis=0)
+                img_array = preprocess_input(img_array)
 
+                # Özellik vektörünü çıkarma
+                features = model.predict(img_array)
+                images.append(features.flatten())  # Özellik vektörünü düzleştirme
+                labels.append(class_folder)
+    return np.array(images), np.array(labels)
 
-# Sınıflar
-classes = ['Monkeypox_augmented', 'Others_augmented']  # 'Monkey Pox' için '0', 'Others' için '1'
+# Veri seti dizini
+data_folder = 'C:/Users/lenovo/Downloads/Fold1/Fold1/Fold1'
 
-# Özellik vektörleri ve etiketleri depolamak için boş listeler
-X_data, y_data = [], []
+# VGG16 modelini yükleyip özellik vektörlerini çıkartma
+base_model = VGG16(weights='imagenet', include_top=False)
+model = Model(inputs=base_model.input, outputs=base_model.get_layer('block5_pool').output)
 
-# Her sınıftaki verileri yükle
-for class_name in classes:
-    class_folder = os.path.join(data_folder, class_name)
+# Eğitim veri setini yükleme ve özellik vektörlerini çıkartma
+train_data_folder = os.path.join(data_folder, 'Train')
+images_train, labels_train = load_and_preprocess_images(train_data_folder, model)
 
-    # Her bir veri dosyasını yükle
-    for file_name in os.listdir(class_folder):
-        if file_name.endswith('.npy'):
-            file_path = os.path.join(class_folder, file_name)
+# Test veri setini yükleme ve özellik vektörlerini çıkartma
+test_data_folder = os.path.join(data_folder, 'Test')
+images_test, labels_test = load_and_preprocess_images(test_data_folder, model)
 
-            # Özellik vektörleri ve etiketleri yükle
-            features = np.load(file_path)
-            labels = np.full(len(features), int(class_name))
+# Etiketleri sayısal değerlere dönüştürme
+label_encoder = LabelEncoder()
+encoded_labels_train = label_encoder.fit_transform(labels_train)
+encoded_labels_test = label_encoder.transform(labels_test)
 
-            # Listelere ekle
-            X_data.extend(features)
-            y_data.extend(labels)
+# Eğitim ve test setlerini oluşturma
+X_train, X_test, y_train, y_test = images_train, images_test, encoded_labels_train, encoded_labels_test
 
-# Veriyi karıştırın
-combined = list(zip(X_data, y_data))
+# Özellik seçimi
+k_best = SelectKBest(f_classif, k=150)
+X_train_selected = k_best.fit_transform(X_train, y_train)
+X_test_selected = k_best.transform(X_test)
 
-# Kontrol et: Eğer veri seti boşsa, hatayı önleyin
-if combined:
-    np.random.shuffle(combined)
-    X_data[:], y_data[:] = zip(*combined)
-else:
-    print("Veri seti boş, karıştırma işlemi yapılamıyor.")
+# Veri standardizasyonu
+scaler = StandardScaler()
+X_train_std = scaler.fit_transform(X_train_selected)
+X_test_std = scaler.transform(X_test_selected)
 
-# Veriyi eğitim ve test kümelerine ayırın
-X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.2, random_state=42)
+# Destek Vektör Makineleri (SVM) modelini oluşturma ve eğitme
+svm_classifier = SVC(kernel='linear', C=1.0)
+svm_classifier.fit(X_train_std, y_train)
 
+# Test seti üzerinde modeli değerlendirme
+y_pred = svm_classifier.predict(X_test_std)
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Test Accuracy: {accuracy}')
 
+# Sınıflandırma raporu
+print(classification_report(y_test, y_pred, target_names=label_encoder.classes_))
 
-# Sınıf ağırlıklarını belirleme
-class_weights = {0: 1.0, 1: 2.0}  # Bu örnekte, 'Monkey Pox' sınıfı için 1.0, 'Others' sınıfı için 2.0
-
-# Destek Vektör Makineleri (SVM) sınıflandırma modelini oluşturun
-model = SVC(kernel='linear', class_weight=class_weights, random_state=42)
-
-# Modeli eğitin
-model.fit(X_train, y_train)
-
-# Test kümesi üzerinde modelin performansını değerlendirin
-y_pred = model.predict(X_test)
-
-# Sınıflandırma raporu ve karışıklık matrisi
-print("Classification Report:")
-print(classification_report(y_test, y_pred))
-print("Confusion Matrix:")
-print(confusion_matrix(y_test, y_pred))
+# Confusion Matrix
+cm = confusion_matrix(y_test, y_pred)
+plt.figure(figsize=(8, 6))
+sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=label_encoder.classes_, yticklabels=label_encoder.classes_)
+plt.title('Confusion Matrix')
+plt.xlabel('Predicted')
+plt.ylabel('True')
+plt.show()
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Monkeypox
+ # SVM ile Görüntü Sınıflandırma
+
+Bu proje, Support Vector Machines (SVM) kullanarak monkeypox hastalığı görüntülerini benzer hastalıklardan ayırmak için sınıflandırma modeli oluşturmayı gösterir. Proje, özellikle VGG16 modeli kullanılarak elde edilen özellik vektörleri üzerinde çalışır ve SVM ile bu özellik vektörlerini kullanarak sınıflandırma yapar.
+
+## Veri Seti
+
+Bu projede kullanılan veri seti "Fold1" adlı bir klasör içindedir. Bu klasörün altında "Train" ve "Test" klasörleri bulunmaktadır.
+
+## Accuracy
+
+Modelin eğitim ve test doğruluk değeri 0.9111111111111111"dur. Hiç de fena değil :D
